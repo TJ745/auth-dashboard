acaafa3d0cd6e146ebb7dd3767801bfd24613e01
TJ745 <talha_mughalz@hotmail.com>
2025-10-01 17:35:59 +0300

    Fixed Prisma lib

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,12 +6,12 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient({ log: ["query"] });
 } else {
-  // @ts-expect-error
+  // @ts-expect-error -- allow global variable
   if (!global.__prisma) {
-    // @ts-expect-error
+    // @ts-expect-error -- allow global variable
     global.__prisma = new PrismaClient({ log: ["query"] });
   }
-  // @ts-expect-error
+  // @ts-expect-error -- allow global variable
   prisma = global.__prisma;
 }
 
